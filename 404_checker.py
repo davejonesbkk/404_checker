cc9d69e7bd8b9e9c2d0a25e55a15d96154ebd0b2
David Jones <davejonesbkk@gmail.com>
2017-01-05 08:23:21 +0700

    second commit

--- 404_checker.py
+++ 404_checker.py
@@ -16,8 +16,10 @@ def send_mail(msg):
 	server.set_debuglevel(1)
 	server.quit()
 
-
-#Get the list of urls from a file 
+"""
+Get the list of urls from a file (this should be changed to 
+your own .txt file with your urls in that you want to track)
+"""
 
 fp = open('test_urls.txt', 'r')
 
@@ -43,7 +45,7 @@ for url in urls:
 
 		subject = 'Some linked pages are now 404s'
 
-		msg_text = 'You have 404s from some links'
+		msg_text = 'The following pages are now 404s: %r\n' %(dead_urls)
 
 		msg = 'Subject: %s\n\n%s' % (subject, msg_text)
 
